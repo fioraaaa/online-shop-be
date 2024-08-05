dcb0069483ead07e3098bd40b041b12918c23b75
fioraaaa <fiora.berliana.tif420@polban.ac.id>
2024-08-05 14:48:24 +0700

    add minio

--- src/main/java/com/project/online_shop_be/OnlineShopBeApplication.java
+++ src/main/java/com/project/online_shop_be/OnlineShopBeApplication.java
@@ -2,8 +2,10 @@ package com.project.online_shop_be;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.project.online_shop_be", "lib.minio", "lib.i18n"})
 public class OnlineShopBeApplication {
 
 	public static void main(String[] args) {
